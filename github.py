7fc56cadae4288d5777ead653583ea0bbd9370ab
Özlem Dilek Acar <acarozlemdilek@gmail.com>
2023-07-06 01:36:52 +0300

    update

--- github.py
+++ github.py
@@ -1,7 +1,6 @@
 from userinfo import username, password
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
 import time
 
 class Github:
@@ -9,6 +8,7 @@ class Github:
         self.browser = webdriver.Chrome()
         self.username = username
         self.password = password
+        self.followers = []
 
 
     def signIn(self):
@@ -23,7 +23,47 @@ class Github:
         self.browser.find_element(By.NAME, "commit").click()
 
 
+    def loadFollowers(self):
+        items = self.browser.find_elements(By.CSS_SELECTOR, ".d-table.table-fixed")
+        for i in items:
+            self.followers.append(i.find_element(By.CSS_SELECTOR,".Link--secondary").text)
+
+
+    def getFollowers(self):
+        self.browser.get(f"https://github.com/{self.username}?tab=followers")
+        time.sleep(2)
+
+        self.loadFollowers()
+
+        while True:
+            links = self.browser.find_element(By.CSS_SELECTOR, ".pagination").find_elements(By.TAG_NAME, "a")
+
+            if len(links) == 1:
+                if links[0].text == "Next":
+                    links[0].click()
+                    time.sleep(1)
+                    self.loadFollowers()
+
+
+                else:
+                    break
+            
+            else:
+                for link in links:
+                    if link.text == "Next":
+                        link.click()
+                        time.sleep(1)
+                        
+                        self.loadFollowers()
+
+
+                    else:
+                        continue
+
 print(username, password)
 github = Github(username, password)
 github.signIn()
+github.getFollowers()
+print(github.followers)
+print(len(github.followers))
 time.sleep(3)
